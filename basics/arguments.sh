f8fbd07086c6ea85f487ac33371e4e6127fc2e6b
jctiru <jonathanchristiantiru@gmail.com>
2020-04-26 21:48:29 +0800

    Create arguments example

---
+++ basics/arguments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Script name: $0"
+echo "First arg: $1"
+echo "Second arg: $2"
+echo "All args with \$@: $@"
+echo "All args with \$*: $*" # Single string with IFS
+echo "Args count: $#"
