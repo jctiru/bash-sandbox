6d38bb723f69b15a3e0af96a0ba7ae682a55838b
jctiru <jonathanchristiantiru@gmail.com>
2020-04-26 21:55:27 +0800

    Modify IFS in arguments example

--- basics/arguments.sh
+++ basics/arguments.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# To see IFS - set | grep ^IFS
+# Result usually IFS=$' \t\n'
+IFS=","
 echo "Script name: $0"
 echo "First arg: $1"
 echo "Second arg: $2"
 echo "All args with \$@: $@"
-echo "All args with \$*: $*" # Single string with IFS
+echo "All args with \$*: $*" # Single string with first char of IFS
 echo "Args count: $#"
