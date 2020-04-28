a4837b6cd22c1ffc187ba8a9bd0b20d38f88285d
jctiru <jonathanchristiantiru@gmail.com>
2020-04-28 21:56:41 +0800

    Modify for loop example to only loop on file*.txt files

--- loops/for_loop.sh
+++ loops/for_loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# In all .txt files in current directory, add a new line with current date and first 5 lines of ps command
-for FILE in *.txt
+# In all file*.txt files in current directory, add a new line with current date and first 5 lines of ps command
+for FILE in file*.txt
 do
   echo $(date) >> $FILE
   ps -ef | head -5 >> $FILE
